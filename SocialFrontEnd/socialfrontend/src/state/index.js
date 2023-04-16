68b3d5deebe23ad9565ec196d9d2f16435cddac8
Prafull Pandey <prafullpandey62000@gmail.com>
2023-04-16 23:12:41 +0530

    navbar component

--- SocialFrontEnd/socialfrontend/src/state/index.js
+++ SocialFrontEnd/socialfrontend/src/state/index.js
@@ -2,8 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     mode: "light",
-    user: null,
-    token: null,
+    user: "prafull", // inital null
+    token: null, 
     posts: [],
 };
 
