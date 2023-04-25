fe1cbb757ba70fbd40c1602a4420da2064a64442
himanshudigrase <himanshudigrase@gmail.com>
2023-04-26 00:53:22 +0530

    AddedMongoURL

--- SocialBackend/index.js
+++ SocialBackend/index.js
@@ -61,7 +61,7 @@ app.use("/posts",postRoutes);
 
 /* MONGOOSE Setup*/
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.PORT,{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,  
 })
