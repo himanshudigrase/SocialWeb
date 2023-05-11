7861318e062368443937fbfb756e5237daaf5061
himanshudigrase <himanshudigrase@gmail.com>
2023-05-11 23:08:40 +0530

    Commented Logstash in logger

--- SocialBackend/logger.js
+++ SocialBackend/logger.js
@@ -1,7 +1,6 @@
 import { createLogger, transports, format } from "winston";
 import {ElasticsearchTransport} from 'winston-elasticsearch';
-const LogstashTransport = require('winston-logstash-transport');
-
+import {LogstashTransport} from 'winston-logstash-transport';
 
 const { combine, timestamp, label, printf,prettyPrint } = format;
 const CATEGORY = "winston custom format";
@@ -15,23 +14,23 @@ const CATEGORY = "winston custom format";
 
 const logger = createLogger({
   level: "info",
-  format: combine(
-    label({ label: CATEGORY }),
-    timestamp({
-        format: "MMM DD YYYY HH:mm:ss",
-      }),
-      prettyPrint()
-    ),
+  // format: combine(
+  //   label({ label: CATEGORY }),
+  //   timestamp({
+  //       format: "MMM DD YYYY HH:mm:ss",
+  //     }),
+  //     prettyPrint()
+  //   ),
   //logger method...
   transports: [
     //new transports:
-    new transports.File({
-      filename: "./logs.log",
-    }),
-    new LogstashTransport({
-      host: 'logstash',
-      port: 9600,
-    }),
+    // new transports.File({
+    //   filename: "./logs.log",
+    // }),
+    // new LogstashTransport({
+    //   host: 'logstash',
+    //   port: 9600,
+    // }),
     new ElasticsearchTransport({
       level: 'info',
       index: 'logs',
