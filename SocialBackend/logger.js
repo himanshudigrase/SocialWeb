c06ee6c2c2b398f1c728fc357db82185feb0e365
himanshudigrase <himanshudigrase@gmail.com>
2023-05-13 16:13:42 +0530

    Added Screenshots for report

--- SocialBackend/logger.js
+++ SocialBackend/logger.js
@@ -5,38 +5,30 @@ import {LogstashTransport} from 'winston-logstash-transport';
 const { combine, timestamp, label, printf,prettyPrint } = format;
 const CATEGORY = "winston custom format";
 
-// //Using the printf format.
-// const customFormat = printf(({ level, message, label, timestamp }) => {
-//   return `${timestamp} [${label}] ${level}: ${message}`;
-// });
-
-
-
 const logger = createLogger({
   level: "info",
-  // format: combine(
-  //   label({ label: CATEGORY }),
-  //   timestamp({
-  //       format: "MMM DD YYYY HH:mm:ss",
-  //     }),
-  //     prettyPrint()
-  //   ),
+  format: combine(
+    label({ label: CATEGORY }),
+    timestamp({
+        format: "MMM DD YYYY HH:mm:ss",
+      }),
+      prettyPrint()
+    ),
   //logger method...
   transports: [
     //new transports:
-    // new transports.File({
-    //   filename: "./logs.log",
-    // }),
-    // new LogstashTransport({
-    //   host: 'logstash',
-    //   port: 9600,
-    // }),
+    new transports.File({
+      filename: "./logs.log",
+    }),
+    new LogstashTransport({
+      host: 'logstash',
+      port: 9600,
+    }),
     new ElasticsearchTransport({
       level: 'info',
       index: 'logs',
       clientOpts: {
-        node: 'http://localhost:9200/',
-        
+        node: 'http://localhost:9200/',       
       },
     }),
   ],
