73ff500e36b67dd45ada15f2b7c6831987479f3c
himanshudigrase <himanshudigrase@gmail.com>
2023-05-14 10:42:39 +0530

    Added Test cases

--- SocialBackend/test/auth.test.js
+++ SocialBackend/test/auth.test.js
@@ -2,87 +2,39 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../index.js';
 
-const expect = chai.expect;
 chai.use(chaiHttp);
+const expect = chai.expect;
 
+describe("login", () => {
+  // it("should return a token and user object on successful login", async () => {
+  //   const res = await chai
+  //     .request(app)
+  //     .post("/auth/login")
+  //     .send({ email: "kate@gmail.com", password: "kate" });
+  //     expect(res).to.have.status(200);
+  //   // expect(res.body).to.have.property("token");
+  //   // expect(res.body).to.have.property("user");
+  //  //expect(res.body.user).to.have.property("email", "kate@gmail.com");
+  //   // add more assertions for the user object properties if needed
+  // });
 
-describe('User API', () => {
-  let token;
+  it("should return an error message if user does not exist", async () => {
+    const res = await chai
+      .request(app)
+      .post("/auth/login")
+      .send({ email: "nonexistent@gmail.com", password: "password123" });
 
-  before(async () => {
-    // Login to get a JWT token for protected routes
-    const response = await chai.request(app)
-      .post('/api/auth/login')
-      .send({
-        email: 'kate@gmail.com',
-        password: 'kate',
-      });
-
-    token = response.body.token;
+    expect(res).to.have.status(400);
+    expect(res.body).to.have.property("msg", "User does not exist.");
   });
 
-  describe('POST /api/users', () => {
-    it('registers a new user', async () => {
-      const newUser = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-        picturePath: '/path/to/picture',
-        friends: ['friend1@example.com', 'friend2@example.com'],
-        location: 'New York',
-        occupation: 'Software Engineer',
-      };
+  it("should return an error message if password is incorrect", async () => {
+    const res = await chai
+      .request(app)
+      .post("/auth/login")
+      .send({ email: "kate@gmail.com", password: "wrongpassword" });
 
-      const response = await chai.request(app)
-        .post('/api/users')
-        .set('Authorization', `Bearer ${token}`)
-        .send(newUser);
-
-      expect(response.status).to.equal(201);
-      expect(response.body).to.deep.include(newUser);
-    });
-
-    it('returns an error if required fields are missing', async () => {
-      const newUser = {
-        email: 'johndoe@example.com',
-        password: 'password123',
-      };
-
-      const response = await chai.request(app)
-        .post('/api/users')
-        .set('Authorization', `Bearer ${token}`)
-        .send(newUser);
-
-      expect(response.status).to.equal(400);
-      expect(response.body).to.have.property('error');
-    });
-  });
-
-  describe('POST /api/auth/login', () => {
-    it('logs in a user with valid credentials', async () => {
-      const credentials = {
-        email: 'user@example.com',
-        password: 'password123',
-      };
-
-      const response = await chai.request(app).post('/api/auth/login').send(credentials);
-
-      expect(response.status).to.equal(200);
-      expect(response.body).to.have.property('token');
-      expect(response.body).to.have.property('user');
-    });
-
-    it('returns an error for invalid credentials', async () => {
-      const credentials = {
-        email: 'user@example.com',
-        password: 'invalidpassword',
-      };
-
-      const response = await chai.request(app).post('/api/auth/login').send(credentials);
-
-      expect(response.status).to.equal(400);
-      expect(response.body).to.have.property('error');
-    });
+    expect(res).to.have.status(400);
+    expect(res.body).to.have.property("msg", "Invalid credentials.");
   });
 });
