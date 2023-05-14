727758177f99df96093051236badeffb0b637415
himanshudigrase <himanshudigrase@gmail.com>
2023-05-14 11:47:00 +0530

    Modified Docker compose

--- SocialFrontEnd/socialfrontend/vite.config.js
+++ SocialFrontEnd/socialfrontend/vite.config.js
@@ -5,28 +5,28 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-<<<<<<< HEAD
-    port:8889
-  },
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment'
-  },
-  // use babel to transpile jsx files
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-  rollupInputOptions: {
-    plugins: [
-      require('@rollup/plugin-babel').default({
-        babelHelpers: 'bundled',
-        exclude: 'node_modules/**',
-      }),
-    ],
-  },
-=======
+// <<<<<<< HEAD
+//     port:8889
+//   },
+//   esbuild: {
+//     jsxFactory: 'React.createElement',
+//     jsxFragment: 'React.Fragment'
+//   },
+//   // use babel to transpile jsx files
+//   optimizeDeps: {
+//     include: ['react', 'react-dom'],
+//   },
+//   rollupInputOptions: {
+//     plugins: [
+//       require('@rollup/plugin-babel').default({
+//         babelHelpers: 'bundled',
+//         exclude: 'node_modules/**',
+//       }),
+//     ],
+//   },
+// =======
     port:6002,
     host: '127.0.0.1'
   }
->>>>>>> c06ee6c2c2b398f1c728fc357db82185feb0e365
+// >>>>>>> c06ee6c2c2b398f1c728fc357db82185feb0e365
 })
